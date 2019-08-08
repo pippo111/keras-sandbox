9d073f7ad2a9af4b11e737e959498d7d3982c6c6
Filip Barańczuk <f.baranczuk@gmail.com>
2019-08-08 17:10:01 +0200

    Add jupyter support

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM tensorflow/tensorflow:latest-gpu-py3
+FROM tensorflow/tensorflow:latest-gpu-py3-jupyter
 
 WORKDIR /usr/src/app
 
--- docker-run.sh
+++ docker-run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $(pwd):/usr/src/app --runtime=nvidia -it --rm --user $(id -u):$(id -g) fbdev/ml-sandbox:latest bash
+docker run -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $(pwd):/usr/src/app --runtime=nvidia -it --rm --user $(id -u):$(id -g) -p 8888:8888 fbdev/ml-sandbox:latest bash
