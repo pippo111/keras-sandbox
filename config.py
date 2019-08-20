5445f43e7e81116af04741c8f3fdd6ecd5d733b2
Filip Barańczuk <f.baranczuk@gmail.com>
2019-08-20 13:18:11 +0200

    Add some significant offset to output sample

--- config.py
+++ config.py
@@ -5,15 +5,20 @@ filters = 8
 batch_norm = True
 checkpoint = f'{arch}_{loss_fn}_f{filters}_bs{batch_size}_bn{batch_norm}'
 
+# Brain 48x64x64 offsets: 27, 26, 42
+# Brain 96x128x128 offsets: 45, 44, 84
+# Left / right lateral ventricles 48x64x64 offsets: 27, 26, 42
+# Left / right lateral ventricles 96x128x128 offsets: 45, 44, 84
+
 # Dataset setup
 dataset = {
-  'collection_name': 'mindboggle_84_64x64x48',
+  'collection_name': 'mindboggle_84_64x64x48_brain',
   'input_label_niftii': 'aseg-in-t1weighted_2std.nii.gz',
   'input_image_niftii': 't1weighted_2std.nii.gz',
   'width': 48,
   'height': 64,
   'depth': 64,
-  'labels': [0.0],
+  'labels': [4.0, 43.0],
   'limit': None
 }
 
@@ -22,27 +27,10 @@ model = {
   'arch': arch,
   'loss_fn': loss_fn,  # binary | dice | wce
   'checkpoint': checkpoint,
-  'epochs': 50,
+  'epochs': 1,
   'batch_size': batch_size,
   'seed': 1,
   'threshold': 0.5,
   'filters': filters,
   'batch_norm': batch_norm
 }
-
-# Output result setup
-output = {
-  # 'slice_numbers': [0,1,2,3,4,5],
-  'slice_numbers': [511],
-  'save_slice': False
-}
-
-# Agrs for data augmentation
-generator_args = dict(
-  # horizontal_flip=True,
-  # vertical_flip=True,
-  rotation_range=5,
-  width_shift_range=0.1,
-  height_shift_range=0.1,
-  # zoom_range=0.05
-)
