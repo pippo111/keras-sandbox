fd97b76dc93500622bab1dc49b6a4606f1659ea8
Filip Barańczuk <f.baranczuk@gmail.com>
2019-08-26 15:04:36 +0200

    Test dataset

--- test.py
+++ test.py
@@ -1,40 +1,23 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import numpy as np
 
 from common import model
 from common import dataset
+from common.utils import get_all_gen_items
 
 import matplotlib
 import matplotlib.pyplot as plt
 
 import config as cfg
 
-
-# In[2]:
-
-
 my_dataset = dataset.MyDataset(
     collection_name=cfg.dataset['collection_name']
 )
 
-
-# In[3]:
-
-
-train_generator, test_generator = my_dataset.create_train_test_gen()
+train_generator, valid_generator, test_generator = my_dataset.create_train_valid_test_gen()
+test_image, test_mask = get_all_gen_items(test_generator)
 test_image, test_mask = test_generator.__getitem__(0)
 
-
-# In[4]:
-
-
 image = test_image[0].squeeze()
-
 
 image_0 = image[cfg.logs['axis_0'], :, :]
 image_1 = image[:, cfg.logs['axis_1'], :]
@@ -42,22 +25,22 @@ image_2 = image[:, :, cfg.logs['axis_2']]
 
 image.shape
 
-
-# In[5]:
-
-
 mask = test_mask[0].squeeze()
 
+width, height, depth = cfg.dataset['input_shape']
+
+for i in range(width):
+    for j in range(height):
+        for k in range(depth):
+            # print(mask[i,j,k].max())
+            if mask[i,j,k] > 0:
+                print(i,j,k, mask[i,j,k])
 
 mask_0 = mask[cfg.logs['axis_0'], :, :]
 mask_1 = mask[:, cfg.logs['axis_1'], :]
 mask_2 = mask[:, :, cfg.logs['axis_2']]
 
 mask.shape
-
-
-# In[6]:
-
 
 fig, ax = plt.subplots(2, 3, figsize=(20, 20))
 
@@ -70,7 +53,3 @@ ax[1][1].imshow(np.rot90(mask_1), cmap='gray')
 ax[1][2].imshow(np.rot90(mask_2), cmap='gray')
 
 plt.show()
-# In[ ]:
-
-
-
