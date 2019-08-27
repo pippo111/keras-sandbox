1a3582f64dd3f341221bf3d266d17418fec50c15
Filip Barańczuk <f.baranczuk@gmail.com>
2019-08-27 21:31:09 +0200

    Fix fp/fn rates

--- common/utils.py
+++ common/utils.py
@@ -105,31 +105,29 @@ def crop_to_fit(inputs, outputs, n_layers=4):
 def calc_confusion_matrix(mask, pred):
     combined = mask * 2 + pred
 
-    fpr_array = np.array([]) # false positive ratio (reds)
-    fpr_total = np.array([]).astype('int') # false positive total pixels
-    fnr_array = np.array([]) # false negative ratio (yellows)
-    fnr_total = np.array([]).astype('int') # false negative total pixels
+    fp_total = 0 # false positive total pixels
+    fn_total = 0 # false negative total pixels
+    tn_total = 0 # true negative total pixels
+    tp_total = 0 # true positive total pixels
 
     for image in combined:
         fp = np.count_nonzero(image == 1.0) # false positive (red)
         fn = np.count_nonzero(image == 2.0) # false negative (yellow)
         tp = np.count_nonzero(image == 3.0) # true positive (green)
         tn = np.count_nonzero(image == 0.0) # true negative (black)
-        
-        fpr = fp / (fp + tn) if tn != 0 or fp != 0 else 0
-        fnr = fn / (fn + tp) if tp != 0 or fn != 0 else 0
 
-        fpr_array = np.append(fpr_array, fpr)
-        fpr_total = np.append(fpr_total, fp)
-        fnr_array = np.append(fnr_array, fnr)
-        fnr_total = np.append(fnr_total, fn)
+        fp_total += fp
+        fn_total += fn
+        tn_total += tn
+        tp_total += tp
 
-    fpr_perc = '{:.0%}'.format(fpr_array.mean())
-    fpr_sum = fpr_total.sum()
-    fnr_perc = '{:.0%}'.format(fnr_array.mean())
-    fnr_sum = fnr_total.sum()
+    fpr_perc = fp_total / (fp_total + tn_total) if tn_total != 0 or fp_total != 0 else 0
+    fnr_perc = fn_total / (fn_total + tp_total) if tp_total != 0 or fn_total != 0 else 0
 
-    return fpr_perc, fnr_perc, fpr_sum, fnr_sum
+    fpr_perc = '{:.0%}'.format(fpr_perc)
+    fnr_perc = '{:.0%}'.format(fnr_perc)
+
+    return fpr_perc, fnr_perc, fp_total, fn_total
 
 def get_all_gen_items(generator):
     X_items = list()
