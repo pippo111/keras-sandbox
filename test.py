001f312bf1bc493f67d8ecdb7f4bb40b374ecd07
Filip Barańczuk <f.baranczuk@gmail.com>
2019-09-05 17:58:18 +0200

    Fix recall

--- test.py
+++ test.py
@@ -15,26 +15,29 @@ my_dataset = dataset.MyDataset(
 
 train_generator, valid_generator, test_generator = my_dataset.create_train_valid_test_gen()
 test_image, test_mask = get_all_gen_items(test_generator)
-test_image, test_mask = test_generator.__getitem__(0)
 
-image = test_image[0].squeeze()
+image = test_image[15].squeeze()
 
 image_0 = image[cfg.logs['axis_0'], :, :]
 image_1 = image[:, cfg.logs['axis_1'], :]
 image_2 = image[:, :, cfg.logs['axis_2']]
 
-image.shape
+print(image.shape)
 
-mask = test_mask[0].squeeze()
+test_mask = test_mask.squeeze().argmax(-1)
+mask = test_mask[15]
+print(mask.shape)
+
+print(mask.shape)
 
 width, height, depth = cfg.dataset['input_shape']
 
-for i in range(width):
-    for j in range(height):
-        for k in range(depth):
-            # print(mask[i,j,k].max())
-            if mask[i,j,k] > 0:
-                print(i,j,k, mask[i,j,k])
+for h in range(64):
+    for i in range(width):
+        for j in range(height):
+            for k in range(depth):
+                if test_mask[h,i,j,k] > 0:
+                    print(h,i,j,k, test_mask[h,i,j,k])
 
 mask_0 = mask[cfg.logs['axis_0'], :, :]
 mask_1 = mask[:, cfg.logs['axis_1'], :]
