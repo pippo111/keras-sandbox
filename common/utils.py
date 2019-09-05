92b7c7ec60639372f4f7b729c8eaff3c47137711
Filip Barańczuk <f.baranczuk@gmail.com>
2019-09-05 16:08:03 +0200

    Precision metric

--- common/utils.py
+++ common/utils.py
@@ -139,6 +139,25 @@ def calc_confusion_matrix(mask, pred):
 
     return fpr_perc, fnr_perc, fp_total, fn_total, f_total
 
+def calc_precision(mask, pred):
+    mask = mask.argmax(axis=-1)
+    pred = pred.argmax(axis=-1)
+    combined = mask * 2 + pred
+
+    fp_total = 0 # false positive total pixels
+    tp_total = 0 # true positive total pixels
+
+    for image in combined:
+        fp = np.count_nonzero(image == 1.0) # false positive (red)
+        tp = np.count_nonzero(image == 3.0) # true positive (green)
+
+        fp_total += fp
+        tp_total += tp
+
+    precision = tp_total / (tp_total + fp_total)
+
+    return precision
+
 def get_all_gen_items(generator):
     X_items = list()
     y_items = list()
